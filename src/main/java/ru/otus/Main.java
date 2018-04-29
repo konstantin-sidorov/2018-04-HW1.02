f0b9cb53414d44670f247ac00f5dfccdd82a8a4d
Konstantin <sks79@yandex.ru>
2018-04-30 01:33:49 +0400

    3

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -1,7 +1,10 @@
 package ru.otus;
 
-/**
- * Created by Konstantin on 29.04.2018.
- */
+
 public class Main {
+    public static void main(String[] args) throws IllegalAccessException{
+        System.out.println("Размер Empty String:"+ObjectSizeCalculator.sizeOf(new String()));
+        System.out.println("Размер Object:"+ObjectSizeCalculator.sizeOf(new Object()));
+        System.out.println("Размер Array:"+ObjectSizeCalculator.sizeOf(new Object[0]));
+    }
 }
